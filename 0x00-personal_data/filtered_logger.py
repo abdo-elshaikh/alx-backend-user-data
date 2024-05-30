1cc9fbaae781c1b08697ac8ec00e8635ef793bf8
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-05-30 17:37:01 +0300

    complete personal data tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,7 +44,7 @@ def get_logger() -> logging.Logger:
     logger.setLevel(logging.INFO)
     logger.propagate = False
     stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
+    formatter = RedactingFormatter(list(PII_FIELDS))
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
