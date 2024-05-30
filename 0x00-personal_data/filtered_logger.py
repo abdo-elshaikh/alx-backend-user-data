e764bf3b219e718e613b739fe98808b0abe652cc
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-05-30 17:39:30 +0300

    complete personal data tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,15 +24,14 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        '''Filters values in incoming log records using filter_datum'''
+        """ Returns filtered values from log records """
         return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
+                            super().format(record), self.SEPARATOR)
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
