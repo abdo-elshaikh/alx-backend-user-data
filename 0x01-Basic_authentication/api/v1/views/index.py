c16d9135ee33995ec6fcc8545f528b191a587ca3
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-06-04 18:57:16 +0300

    complete basic auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,9 +28,15 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-  """GET /api/vi/unauthorized
+    """ GET /api/v1/unauthorized
+    Return - raises 401 error
+    """
+    abort(401)
 
-  Returns:
-      str: 
-  """
-  abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return - raises 403 error
+    """
+    abort(403)
